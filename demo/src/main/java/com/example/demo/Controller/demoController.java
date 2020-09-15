306e5f42d10c00d7a013f6efd049342effd21beb
scattter <413365742@qq.com>
2020-09-15 11:24:10 +0800

    [zk] finsh basic structure

--- demo/src/main/java/com/example/demo/Controller/demoController.java
+++ demo/src/main/java/com/example/demo/Controller/demoController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class testController {
+public class demoController {
     @GetMapping("/test")
     String testProject(){
         return "success";

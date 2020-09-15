8465bda142f7400f50cd9861fe67725b7020f7d8
scattter <413365742@qq.com>
2020-09-15 14:42:28 +0800

    [zk] enable CORS

--- demo/src/main/java/com/example/demo/Controller/EducationController.java
+++ demo/src/main/java/com/example/demo/Controller/EducationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users/{userId}")
 public class EducationController {
     private final EducationService educationService;
--- demo/src/main/java/com/example/demo/Controller/PersonController.java
+++ demo/src/main/java/com/example/demo/Controller/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("users")
 public class PersonController {
 
